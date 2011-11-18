a67a8d0fb1404e5b2bc1a06e7564ba58326b83d3
Kazuhiro Osawa <yappo@shibuya.pl>
2011-11-18 13:09:32 +0900

    initial commit

---
+++ lib/App/Kuragepo.pm
@@ -0,0 +1,32 @@
+package App::Kuragepo;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Kuragepo -
+
+=head1 SYNOPSIS
+
+    use App::Kuragepo;
+
+=head1 DESCRIPTION
+
+App::Kuragepo is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
